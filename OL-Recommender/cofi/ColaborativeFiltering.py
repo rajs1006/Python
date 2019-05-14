37393774d05a7b7f0335c1e904f787b6e6304c26
sourabhraj <sourabh.raj@tu-berlin.de>
2019-05-15 01:32:18 +0200

    Changes : Vectorization

--- OL-Recommender/cofi/ColaborativeFiltering.py
+++ OL-Recommender/cofi/ColaborativeFiltering.py
@@ -22,21 +22,12 @@ class CollaborativeFilter:
         X = np.reshape(params[0:numMovies * numFeature], (numMovies, numFeature))
         Theta = np.reshape(params[numMovies * numFeature:], (numUser, numFeature))
 
-        X_grad = np.zeros(X.shape)
-        Theta_grad = np.zeros(Theta.shape)
-
-        for i in range(0, numMovies):
-            idx = np.where(R[i, :] == 1)
-            ThetaTemp = Theta[idx, :]
-            YTemp = Y[i, idx]
-            X_grad[i, :] = (ThetaTemp.dot(X[i, :]) - YTemp).dot(ThetaTemp) + lam * X[i, :]
-
-        for i in range(0, numUser):
-            idx = np.where(R[:, i] == 1)
-            XTemp = X[idx, :]
-            YTemp = Y[idx, i]
-            Theta_grad[i, :] = (XTemp.dot(Theta[i, :]) - YTemp).dot(XTemp) + lam * Theta[i, :]
+        grad = np.multiply((np.dot(X, Theta.T) - Y), R)
+        # gradients
+        X_grad = np.dot(grad, Theta) + lam * X
+        Theta_grad = np.dot(grad.T, X) + lam * Theta
 
         params = np.concatenate((X_grad, Theta_grad)).ravel()
 
         return params
+
